684b39c7ba6ec806dacb9e665215cfe10f21eb4f
William J Teskey <willjteskey@gmail.com>
2018-05-03 23:09:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves# code an argument here
-  
+   names.each_with_index do |name, number|
+    puts "#{number+1}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
