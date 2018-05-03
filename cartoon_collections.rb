0edda2842861327bcfcd2c1291ab65a100643218
William J Teskey <willjteskey@gmail.com>
2018-05-03 23:09:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,13 @@ def roll_call_dwarves# code an argument here
 end
 
 def summon_captain_planet# code an argument here
-  
+  planeteer_calls.collect do |call|
+    "#{call.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
